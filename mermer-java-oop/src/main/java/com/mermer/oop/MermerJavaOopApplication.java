0200ee9a1244826826e8bcb8619b6a1b8438c2c9
mermer <mincoln419@naver.com>
2022-10-17 17:53:06 +0900

    oop project init

---
+++ mermer-java-oop/src/main/java/com/mermer/oop/MermerJavaOopApplication.java
@@ -0,0 +1,13 @@
+package com.mermer.oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MermerJavaOopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MermerJavaOopApplication.class, args);
+    }
+
+}
