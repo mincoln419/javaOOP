4fd988b459de5b42dac78777146a473095034128
mermer <mincoln419@naver.com>
2022-10-20 17:32:48 +0900

    refs #7 spring boot 에 기존 비즈니스 로직 추가

---
+++ spring-practice/src/main/java/com/mermer/oop/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.mermer.oop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+}
--- spring-practice/src/test/java/com/mermer/springpractice/SpringPracticeApplicationTests.java
+++ spring-practice/src/test/java/com/mermer/springpractice/SpringPracticeApplicationTests.java
@@ -1,18 +1,10 @@
 package com.mermer.springpractice;
 
-import com.mermer.oop.Main;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 class SpringPracticeApplicationTests {
 
-	@Test
-	void contextLoads() throws IOException {
-		String[] args = {"3", "2" , "1"};
-		Main.main(args);
-	}
 
 }
