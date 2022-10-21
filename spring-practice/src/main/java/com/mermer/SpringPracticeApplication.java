d53e1c913e9e2e11dea229fa36e05960a0ff580c
mermer <mincoln419@naver.com>
2022-10-21 17:20:17 +0900

    application level 설정기능

--- spring-practice/src/main/java/com/mermer/SpringPracticeApplication.java
+++ spring-practice/src/main/java/com/mermer/SpringPracticeApplication.java
@@ -1,14 +1,22 @@
 package com.mermer;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.core.metrics.jfr.FlightRecorderApplicationStartup;
 
 @SpringBootApplication
 public class SpringPracticeApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SpringPracticeApplication.class, args);
+		SpringApplication application = new SpringApplication(SpringPracticeApplication.class);
+		//application.setApplicationStartup(new FlightRecorderApplicationStartup());
+		//application.setApplicationStartup(new BufferingApplicationStartup(1000));
+		//application.setBannerMode(Banner.Mode.OFF);
+
+		application.run(args);
 
 
 	}
